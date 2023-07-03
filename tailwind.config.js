a7c51554493c6c96174644b59cd3c9f7a07a7347
ulugbek-ibrohimov <ulugbekdeveloper90@gmail.com>
2023-07-03 18:06:38 +0500

    Initiall commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+      headerShadow: "0px 15px 15px -10px #ff9f8e"
+      },     
+    },
+  },
+  plugins: [],
+}
+
